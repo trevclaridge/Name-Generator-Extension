38aab8c2077ab44d9ee8ce332a9fbc2673a32e71
Trevor Claridge <trevclaridge@gmail.com>
2023-03-03 17:13:37 -0500

    test typeof chrome

--- web/functions.js
+++ web/functions.js
@@ -1,4 +1,5 @@
 function changeIcon(themeString) {
+  console.log(typeof chrome);
   if (typeof chrome !== "undefined") {
     (async () => {
       const response = await chrome.runtime.sendMessage(themeString);
