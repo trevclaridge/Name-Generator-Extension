f4e1e456deba936e287dd5ce1996706b86e33e93
Trevor Claridge <trevclaridge@gmail.com>
2023-03-03 17:05:30 -0500

    testing web/extension check; thank you chatgpt

--- lib/model/javascript_controller.dart
+++ lib/model/javascript_controller.dart
@@ -4,14 +4,4 @@ class JavascriptController {
   void changeIcon(String themeString) {
     js.context.callMethod('changeIcon', [themeString]);
   }
-
-  // void setSyncPair(String key, String value) {
-  //   js.context.callMethod('setSyncPair', [key, value]);
-  // }
-
-  // String getSyncValue(String key) {
-  //   var value = js.context.callMethod('getSyncValue', [key]);
-  //   // print(value);
-  //   return value;
-  // }
 }
--- web/functions.js
+++ web/functions.js
@@ -1,6 +1,10 @@
 function changeIcon(themeString) {
-  (async () => {
-    const response = await chrome.runtime.sendMessage(themeString);
-    console.log(response);
-  })();
+  if (typeof chrome !== "undefined") {
+    (async () => {
+      const response = await chrome.runtime.sendMessage(themeString);
+      console.log(response);
+    })();
+  } else {
+    console.log("Hello from the web page");
+  }
 }
